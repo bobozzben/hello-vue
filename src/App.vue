4ff1fa2ee615226396e14e365d226c230253afe9
benhserp <ben@hserp.tw>
2021-11-09 11:13:10 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" appvueused="傳入的字串" />
+  <HelloWorld msg="Welcome to Your Vue.js App" appvueused="傳入的字串"/>
 </template>
 
 <script>
